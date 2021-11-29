d952dbc859e862b8d342eeb590f0efb1ae63cdc4
Will Lovell <willard.lovell@icloud.com>
2021-11-29 00:56:39 -0700

    commit tree c44117cb7e6fa40148f7b056dd900dbc69ad7976

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31
+const num2 = 2
+const multiply =  num1 * num2
 
-// Write your code here
+
+
+ let random = Math.floor(Math.random() * 20) + 3;
+
+const num3 = 10
+const num4 = 6
+ let mod = Number.parseInt(num3) - Number.parseInt(num4)
+
+const max = 20
+ Math.max(1,2,3,4,5,6,19,20)
